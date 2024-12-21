1b98eef1eb18e45bed8b2cf796d4381f9902104d
SyntiaNguessom <nguessomsyntia@gmail.com>
2024-12-21 17:01:19 +0100

    test

--- Main.kt
+++ Main.kt
@@ -1,5 +1,5 @@
 
-data class Medecin (val nom : String, val specialité: String){
+class Medecin (val nom : String, val specialité: String){
     var disponibilités : MutableList<String> = mutableListOf()
     fun afficherDisponibilités(){
         if (disponibilités.isEmpty()){
@@ -16,5 +16,8 @@ data class Medecin (val nom : String, val specialité: String){
 
 
 fun main(){
-    val medecin = Medecin("Dr )
+    val medecin = Medecin("Dr.Doogie","Chirugienne" )
+    val creneauxHoraires = listOf("lundi 9h-13H","mercredi 11h-18h", "vendredi 8h-15h")
+    println(" ${medecin.attribuerCreneaux(medecin, creneauxHoraires)}")
+ medecin.afficherDisponibilités()
 }
